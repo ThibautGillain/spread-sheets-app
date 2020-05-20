8443b9580ca31ab6d272d014622ff78e774ee6a0
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2020-05-20 14:16:38 +0200

    Little example with handsontable but key missing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Handsontable from 'handsontable';
+// tslint:disable-next-line:import-spacing
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spread-sheets-app';
+
+  dataset: any[] = [
+    {id: 1, name: 'Ted Right', address: 'Wall Street'},
+    {id: 2, name: 'Frank Honest', address: 'Pennsylvania Avenue'},
+    {id: 3, name: 'Joan Well', address: 'Broadway'},
+    {id: 4, name: 'Gail Polite', address: 'Bourbon Street'},
+    {id: 5, name: 'Michael Fair', address: 'Lombard Street'},
+    {id: 6, name: 'Mia Fair', address: 'Rodeo Drive'},
+    {id: 7, name: 'Cora Fair', address: 'Sunset Boulevard'},
+    {id: 8, name: 'Jack Right', address: 'Michigan Avenue'},
+  ];
 }
