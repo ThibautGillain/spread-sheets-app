6ce2f36c9f2e65cd76d10d83062d38cb97c051a3
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2020-05-20 16:14:43 +0200

    Import materials + spreadsheet interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+
+
+export let SheetHeader = [
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
+  ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ']
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'spread-sheets-app';
+
+  displayedColumns: string[] = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  objects: string[] = ['Ordinateur', 'Baie de brassage', 'Serveur', 'Base de données'];
+  dataSource = new MatTableDataSource(SheetHeader);
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonModule} from '@angular/common';
+import {CdkTableModule} from '@angular/cdk/table';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    CdkTableModule,
+    NoopAnimationsModule,
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
