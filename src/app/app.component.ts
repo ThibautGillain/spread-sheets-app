908802a4a0e92220df0834acd11206020faf7a98
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2020-05-20 18:08:02 +0200

    interface without overflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatTableModule} from '@angular/material/table';
 import {MatSelectModule} from '@angular/material/select';
@@ -6,7 +6,7 @@ import {MatSelectModule} from '@angular/material/select';
 interface Entry {
   value: string;
 }
-
+/*
 export let SheetHeader = [
   ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
   ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
@@ -19,18 +19,50 @@ export let SheetHeader = [
   ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        '],
   ['        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ', '        ']
 ];
-
+*/
+export const inputColumns = 20;
+export const inputRows = 10;
+let SheetHeader = [];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spread-sheets-app';
 
-  displayedColumns: string[] = [' ', 'Produits', 'Nombre'];
+
+  displayedColumns: string[] = [' '];
   objects: Entry[] = [
-    {value: 'Ordinateur'}, {value: 'Baie de brassage'}, {value: 'Serveur'}, {value : 'Base de données'} ];
-  dataSource = new MatTableDataSource(SheetHeader);
+                        {value: 'Ordinateur'},
+                        {value: 'Baie de brassage'},
+                        {value: 'Serveur'},
+                        {value : 'Base de données'}
+                      ];
+
+  dataSource: any;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    for (let j = 1 ; j < inputColumns + 1 ; j++) {
+      this.displayedColumns.push('c' + j.toString());
+    }
+
+    for(let i = 0; i < inputRows; i++) {
+      let line = [];
+      for (let j = 0 ; j < inputColumns ; j++) {
+        const column = ' ';
+        line.push(column);
+      }
+      SheetHeader.push(line);
+    }
+
+    this.dataSource = new MatTableDataSource(SheetHeader);
+  }
+
+
 
 }
